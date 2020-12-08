edbcb2b6797b22d353436961e051b3482c61c7b4
Rachel <racheljay42@gmail.com>
2020-12-08 16:54:26 -0500

    take 3

--- config/database.php
+++ config/database.php
@@ -1,31 +1,16 @@
 <?php
-// $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-// $host = $url["host"] ?? null;
-// $username = $url["user"] ?? null;
-// $password = $url["pass"] ?? null;
-// $database = substr($url["path"], 1);
-// 'mysql' => [
-//             'driver' => 'mysql',
-//             'host' => $host,
-//             'port' => env('DB_PORT', '3306'),
-//             'database' => $database,
-//             'username' => $username,
-//             'password' => $password,
-//             'unix_socket' => env('DB_SOCKET', ''),
-//             'charset' => 'utf8mb4',
-//             'collation' => 'utf8mb4_unicode_ci',
-//             'prefix' => '',
-//             'strict' => true,
-//             'engine' => null,
-//         ]
+
 use Illuminate\Support\Str;
 $url = getenv('JAWSDB_URL');
 $dbparts = parse_url($url);
-$host = $dbparts['host'];
+
+$hostname = $dbparts['host'];
 $username = $dbparts['user'];
 $password = $dbparts['pass'];
 $database = ltrim($dbparts['path'],'/');
+
 return [
+
     /*
     |--------------------------------------------------------------------------
     | Default Database Connection Name
@@ -36,7 +21,9 @@ return [
     | you may use many connections at once using the Database library.
     |
     */
+
     'default' => env('DB_CONNECTION', 'mysql'),
+
     /*
     |--------------------------------------------------------------------------
     | Database Connections
@@ -52,7 +39,9 @@ return [
     | choice installed on your machine before you begin development.
     |
     */
+
     'connections' => [
+
         'sqlite' => [
             'driver' => 'sqlite',
             'url' => env('DATABASE_URL'),
@@ -61,7 +50,6 @@ return [
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
         ],
 
-        //  UNCOMMENT TO TEST LOCALLY
         // 'mysql' => [
         //     'driver' => 'mysql',
         //     'url' => env('DATABASE_URL'),
@@ -82,11 +70,9 @@ return [
         //     ]) : [],
         // ],
 
-        //COMMENT OUT TO TEST LOCALLY
-        
         'mysql' => [
             'driver' => 'mysql',
-            'host' => $host,
+            'host' => $hostname,
             'port' => env('DB_PORT', '3306'),
             'database' => $database,
             'username' => $username,
@@ -95,9 +81,14 @@ return [
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
+            'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
         ],
+
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
@@ -112,6 +103,7 @@ return [
             'schema' => 'public',
             'sslmode' => 'prefer',
         ],
+
         'sqlsrv' => [
             'driver' => 'sqlsrv',
             'url' => env('DATABASE_URL'),
@@ -124,7 +116,9 @@ return [
             'prefix' => '',
             'prefix_indexes' => true,
         ],
+
     ],
+
     /*
     |--------------------------------------------------------------------------
     | Migration Repository Table
@@ -135,7 +129,9 @@ return [
     | the migrations on disk haven't actually been run in the database.
     |
     */
+
     'migrations' => 'migrations',
+
     /*
     |--------------------------------------------------------------------------
     | Redis Databases
@@ -146,12 +142,16 @@ return [
     | such as APC or Memcached. Laravel makes it easy to dig right in.
     |
     */
+
     'redis' => [
+
         'client' => env('REDIS_CLIENT', 'phpredis'),
+
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
             'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
         ],
+
         'default' => [
             'url' => env('REDIS_URL'),
             'host' => env('REDIS_HOST', '127.0.0.1'),
@@ -159,6 +159,7 @@ return [
             'port' => env('REDIS_PORT', '6379'),
             'database' => env('REDIS_DB', '0'),
         ],
+
         'cache' => [
             'url' => env('REDIS_URL'),
             'host' => env('REDIS_HOST', '127.0.0.1'),
@@ -166,5 +167,7 @@ return [
             'port' => env('REDIS_PORT', '6379'),
             'database' => env('REDIS_CACHE_DB', '1'),
         ],
+
     ],
+
 ];
